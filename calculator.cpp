6971cbe82977df1be61e5ad26869434a1f82cfae
haymi <robelwon09@gmail.com>
2025-04-05 11:19:24 +0300

    power opration

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,14 @@ using namespace std;
 double addition(double a, double b){
     return a + b;
 }
+double power(double base, int exponent) {
+    double result = 1.0;
+    bool isNegative = (exponent < 0);
+    exponent = abs(exponent);
+
+    for (int i = 0; i < exponent; ++i) {
+        result *= base;
+    }
 
 
 
@@ -15,10 +23,16 @@ double substraction(double a, double b) {
     return a-b;
 }
 int main() {
+    double base;
+    int exponent;
 
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Enter exponent: ";
+    cin >> exponent;
 
-
-
+    double result = power(base, exponent);
+    cout << base << "^" << exponent << " = " << result << std::endl;
 
     return 0;
 }
